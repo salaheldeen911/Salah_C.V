e1d28d4ec81791eee286fcce308ffa1c5f3d6709
salah <salah.eldeen.mail@gmail.com>
2023-05-26 11:57:01 +0200

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
-function activeSide(arrow) {
-  arrow.classList.add("hidden");
-  document.getElementById("left_arrow_holder").classList.remove("hidden");
-  document.getElementById("sidebar").classList.add("activeSide");
+const SIDEBAR = document.getElementById("sidebar");
+const RIGHT_ARROW = document.getElementById("right_arrow_holder");
+const LEFT_ARROW = document.getElementById("left_arrow_holder");
+
+function activeSide() {
+  if (!SIDEBAR.classList.contains("activeSide")) {
+    RIGHT_ARROW.classList.add("hidden");
+    LEFT_ARROW.classList.remove("hidden");
+    SIDEBAR.classList.add("activeSide");
+  }
 }
 
-function deactiveSide(arrow) {
-  arrow.classList.add("hidden");
-  document.getElementById("right_arrow_holder").classList.remove("hidden");
-  document.getElementById("sidebar").classList.remove("activeSide");
+function deactiveSide() {
+  if (SIDEBAR.classList.contains("activeSide")) {
+    LEFT_ARROW.classList.add("hidden");
+    RIGHT_ARROW.classList.remove("hidden");
+    SIDEBAR.classList.remove("activeSide");
+  }
 }
